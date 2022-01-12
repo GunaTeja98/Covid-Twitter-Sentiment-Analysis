441c4ea63fbaf67fb4026021868f3e69125e7b5c
Venkata Guna Teja <54614015+GunaTeja98@users.noreply.github.com>
2022-01-11 22:50:26 -0800

    Update utils.py

--- Covid Sentiment Analysis/pubsub-pipe-image/utils.py
+++ Covid Sentiment Analysis/pubsub-pipe-image/utils.py
@@ -75,9 +75,7 @@ def cleanup(data):
                 newdict[k] = list(flatten(v))
             elif k == 'created_at' and v:
                 newdict[k] = str(dateutil.parser.parse(v))
-            # temporarily, ignore some fields not supported by the
-            # current BQ schema.
-            # TODO: update BigQuery schema
+       
             elif (k == 'video_info' or k == 'scopes' or k == 'withheld_in_countries'
                   or k == 'is_quote_status' or 'source_user_id' in k
                   or k == ''
